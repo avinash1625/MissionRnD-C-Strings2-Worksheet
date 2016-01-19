06074fa1ca67013a4290a3a47bb9458bd8dfc769
avinash1625 <avinash1625@missionrnd.com>
2016-01-19 20:59:48 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -11,10 +11,46 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 */
 
 #include <stdlib.h>
-
-int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+int strlen(char *str)
+{
+	int i;
+	for (i = 0; str[i]; i++);
+	return i;
 }
+
+int str_cmp(char *str1, int i,int j, char *str2)
+{
+		int k = 0, flag = 1;
+		if (j - i + 1 != strlen(str2))
+			return 0;
+		while (str1[i] != '\0' && str2[k] != '\0')
+		{
+			if (str1[i] != str2[k])
+			{
+				flag= 0;
+				break;
+			}
+			k++;
+			i++;
+		}
+		return flag;
+}
+
+int count_word_in_str_way_1(char *str, char *word)
+{
+		int count = 0, i = 0, len = strlen(word);
+		while (str[i] != '\0')
+		{
+			if (str[i] == word[0]&&str[i+len-1]==word[len-1])
+			{
+				if (str_cmp(str, i,i+len-1, word) == 1)
+					count++;
+			}
+			i++;
+		}
+		return count;
+}
+
 
 int count_word_int_str_way_2_recursion(char *str, char *word){
 	return 0;
