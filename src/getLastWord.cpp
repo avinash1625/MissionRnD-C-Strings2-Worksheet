abeb4c86a988f3ae462d5c477db98d8d1704cc1b
avinash1625 <avinash1625@missionrnd.com>
2016-01-19 20:36:26 +0530

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -11,6 +11,30 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 #include <stdlib.h>
 
 char * get_last_word(char * str){
-
-	return NULL;
+	char *last_word = (char*)malloc(sizeof(char)* 20);
+	int j = 0, i = 0, k = -1;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+			k = i + 1;
+		i++;
+	}
+	if (k == -1)
+	{
+		if (str[0] == ' ')
+			last_word[j] = '\0';
+		else
+			return str;
+	}
+	else
+	{
+		while (str[k] != '\0' && str[k] != ' ')
+		{
+			last_word[j] = str[k];
+			k++;
+			j++;
+		}
+		last_word[j] = '\0';
+	}
+	return last_word;
 }
